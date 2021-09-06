ba98c13899b7296bd83f55c1fb619bd672999677
gloryjumbo <bed-com-23-17@cc.ac.mw>
2021-09-06 16:18:10 +0200

    coommits

--- app/src/main/java/com/example/houserentalsapp/AdminPanel.java
+++ app/src/main/java/com/example/houserentalsapp/AdminPanel.java
@@ -1,14 +1,24 @@
 package com.example.houserentalsapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AdminPanel extends AppCompatActivity {
+    Button btnPanel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_panel);
+
+        btnPanel =  findViewById(R.id.btnPanel3);
+
+        btnPanel.setOnClickListener(view -> {
+            Intent intent = new Intent(AdminPanel.this, CustomerPanel.class);
+            startActivity(intent);
+        });
     }
 }
